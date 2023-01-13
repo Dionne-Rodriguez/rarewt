07fa364c77450ac37ab2e27f6cda8c0fd986f051
Dionne <dionne.r.9@gmail.com>
2023-01-13 14:53:58 -0500

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-dark">
+    <div class="container">
+      <Header />
+      <div class="container" style="height: 2000px">
+        <h1>Welcome to Squadron RARE!</h1>
+        <p>
+          We are a competitive, battle-focused group of players in the game War
+          Thunder.
+        </p>
+        <h2>Contact Us</h2>
+        <p>
+          If you would like to join Squadron RARE or have any questions, you can
+          contact us on Discord:
+        </p>
+        <p>
+          <a :href="discordLink" target="_blank">Join our server</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Header from "./components/Header.vue";
+export default {
+  name: "SquadronRarePage",
+  components: {Header},
+  setup() {
+    const discordLink = ref("https://discord.gg/Ezw7ybbH");
+
+    return {
+      discordLink,
+    };
+  },
+};
+</script>
+
+<style>
+p{
+  color: white;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from "@vue/runtime-core"
+
+export default  defineComponent({
+name:"Header",
+setup() {
+  
+}
+})
+</script>
+
+<template>
+<nav class="navbar navbar-dark bg-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/rare-icon.PNG" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      RARE
+    </a>
+    <a class="nav-link" href="about">Home</a>
+    <a class="nav-link" href="about">About</a>
+    <a class="nav-link" href="players">Players</a>
+    <a class="nav-link" href="news">News</a>
+    <a class="nav-link" href="livestreams">Livestreams</a>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+</style>
