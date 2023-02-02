ff38c058da485e6542e869cf1b49133c63a30819
Dionne <dionnerodriguez.se@gmail.com>
2023-02-01 20:09:02 -0500

    add more players to livestream

--- src/views/Livestreams.vue
+++ src/views/Livestreams.vue
@@ -4,9 +4,9 @@
             <h1 class="pb-5">Livestreams</h1>
         </div>
         <div class="pb-5 mb-5">
-            <h3 class="pb-4">goFlexDickie</h3>
-            <iframe src="https://player.twitch.tv/?channel=goflexdickie&parent=rarewt.com" frameborder="0"
-                allowfullscreen="true" scrolling="no" height="400" width="100%"></iframe>
+            <h3 class="pb-4">ToSupremeOVO</h3>
+            <iframe src="https://player.twitch.tv/?channel=tosupreme&parent=rarewt.com" frameborder="0"
+                allowfullscreen="true" scrolling="no" height="378" width="100%"></iframe>
         </div>
         <div class="pb-5 mb-5">
             <h3 class="pb-4">Class1FYDE</h3>
@@ -14,6 +14,21 @@
                 title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen></iframe>
+        </div>
+        <div class="pb-5 mb-5">
+            <h3 class="pb-4">goFlexDickie</h3>
+            <iframe src="https://player.twitch.tv/?channel=goflexdickie&parent=rarewt.com" frameborder="0"
+                allowfullscreen="true" scrolling="no" height="400" width="100%"></iframe>
+        </div>
+        <div class="pb-5 mb-5">
+            <h3 class="pb-4">Skinny</h3>
+            <iframe src="https://player.twitch.tv/?channel=McSkinnySandwich&parent=rarewt.com" frameborder="0"
+                allowfullscreen="true" scrolling="no" height="378" width="100%"></iframe>
+        </div>
+        <div class="pb-5 mb-5">
+            <h3 class="pb-4">mynamelydia</h3>
+            <iframe src="https://player.twitch.tv/?channel=mynamelydia&parent=rarewt.com" frameborder="0"
+                allowfullscreen="true" scrolling="no" height="378" width="100%"></iframe>
         </div>
     </div>
 </template>
